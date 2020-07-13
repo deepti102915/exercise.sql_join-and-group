56728934e20e3ab68cd8d36dca8eb9fb225c1a69
ANIL CHADDHA <a101981c@ANILs-Air.lan1>
2020-07-13 11:17:59 -0400

    Updated Part1

--- SQL SCRIPTS/Part1.sql
+++ SQL SCRIPTS/Part1.sql
@@ -1,4 +1,4 @@
-Select cus.customerName As Customer_Name, concat(emp.lastName, ',', emp.firstName) As "lastName, firstName"
+Select cus.customerName As Customer_Name, concat(emp.lastName, ',', emp.firstName) As Sales_Rep
   from classicmodels.customers cus Inner Join classicmodels.employees emp
   on cus.salesRepEmployeeNumber=emp.employeeNumber order by 1;
 
